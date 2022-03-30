651004cb894cd95825f99cb91c7aeba9cbe68fbd
khcodesstuff <dmitry.kh4@gmail.com>
2022-03-30 08:35:08 +0300

    Setup routes

--- Server/app.js
+++ Server/app.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const app = express();
 
+// Routes
+app.use('/auth', require('./routes/auth'));
+app.use('/chat', require('./routes/chat'));
+app.use('/profile', require('./routes/profile'));
+
 app.get('/', (req, res) => {
     res.send('<h1>Hello world</h1>');
 })
