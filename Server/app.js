b95bed386988f2767dfa9fe7280bac33482611dc
khcodesstuff <dmitry.kh4@gmail.com>
2022-03-30 08:40:30 +0300

    Cleanup

--- Server/app.js
+++ Server/app.js
@@ -7,8 +7,4 @@ app.use('/auth', require('./routes/auth'));
 app.use('/chat', require('./routes/chat'));
 app.use('/profile', require('./routes/profile'));
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello world</h1>');
-})
-
 app.listen(5000, () => { console.log('Listening at */5000') });
