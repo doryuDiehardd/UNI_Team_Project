2770d6b473d091a047b559c8a27ff59f1cebdbed
khcodesstuff <dmitry.kh4@gmail.com>
2022-04-02 11:10:07 +0300

    Written passport config

---
+++ Server/config/passport.js
@@ -0,0 +1,41 @@
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcryptjs');
+
+const UserModel = require('../models/UserModel');
+
+module.exports = function(passport) {
+    passport.use(
+        new LocalStrategy({ usernameField: 'user_name' }, (user_name, password, done) => {
+            // Mathch user
+            UserModel.findOne({ user_name: user_name })
+                .then(user => {
+                    if (!user){
+                        return done(null, false, {message: 'No user with givent username is registered' });
+                    }
+
+                    // Match password
+                    bcrypt.compare(password, user.password, (err, isMatch) => {
+                        if (err) throw err;
+
+                        if (isMatch){
+                            return done(null, user);
+                        }
+                        else{
+                            return done(null, false, { message: 'Password incorrect' });
+                        }
+                    })
+                })
+                .catch(err => console.log(err));
+        })
+    );
+
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser((id, done) => {
+        UserModel.findById(id, (err, user) => {
+            done(err, user);
+        });
+    });
+}
