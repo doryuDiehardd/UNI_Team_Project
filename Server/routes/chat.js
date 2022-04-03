ce851e4ca7e7b72b4fdba6191e10ac838e623b3f
khcodesstuff <dmitry.kh4@gmail.com>
2022-04-03 14:20:36 +0300

    Implemented POST chat/create

--- Server/routes/chat.js
+++ Server/routes/chat.js
@@ -1,7 +1,38 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('/create', (req, res) => {
+const ChatService = require('../services/ChatService');
+const ValidationService = require('../services/ValidationService');
+
+router.post('/create', async (req, res) => {
+    data = {
+        name: req.body.name || null,
+        pic_path: 'placeholder',
+        owner_id: req.body.owner_id || null,
+        messages: [],
+        related_users: req.body.owner_id ? [ req.body.owner_id ] : null,
+        kicked_users: [],
+        join_requests: []
+    }
+
+    //
+    // Validation
+    //
+
+    const errors = ValidationService.validateData_IsAllFilled(data);
+    if (Object.keys(errors).length){
+        return res.status(422).json(errors);
+    }
+    //
+
+    try{
+        await ChatService.createChat(data);
+    }
+    catch(err) {
+        console.log(err);
+        return res.sendStatus(500);
+    }
+
     res.sendStatus(200);
 });
 
--- Server/services/ChatService.js
+++ Server/services/ChatService.js
@@ -1,5 +1,12 @@
-const ChatService = {
+const ChatModel = require('../models/ChatModel');
 
+const ChatService = {
+    createChat: async (data) => {
+        try{
+            await ChatModel.create(data);
+        }
+        catch(err) { throw err }
+    }
 }
 
 module.exports = ChatService;
