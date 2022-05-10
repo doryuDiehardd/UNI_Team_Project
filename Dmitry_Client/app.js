beca0c38f2464943bbff90700d7f4df384674335
khcodesstuff <dmitry.kh4@gmail.com>
2022-05-10 15:16:43 +0300

    Working login, refactored base axios route

--- Dmitry_Client/app.js
+++ Dmitry_Client/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios').default;
 
 const app = express();
 
@@ -8,6 +9,7 @@ app.use(express.urlencoded({ extended:false }))
 
 app.use('/public', express.static(__dirname + '/public'));
 
+axios.defaults.baseURL = 'http://localhost:5000'
 
 app.use('/', require('./routes/index'));
 app.use('/auth', require('./routes/auth'));
--- Dmitry_Client/routes/auth.js
+++ Dmitry_Client/routes/auth.js
@@ -3,20 +3,51 @@ const router = require('express').Router();
 const axios = require('axios').default;
 
 router.get('/register', (req, res) => {
-    res.render('register');
+    res.render('register_login', {
+        route: 'register'
+    });
 });
 
 router.post('/register', (req, res) => {
-    axios.post('http://localhost:5000/auth/register',{
+    axios.post('/auth/register',{
         ...req.body
     })
     .then(response => {
-        res.send(response.status);
+        if (response.status === 200){
+            res.redirect('/');
+        }
+        else{
+            res.sendStatus(response.status);
+        }
     })
     .catch(err => {
         console.log(err);
         res.sendStatus(500);
+    });
+});
+
+router.get('/login', (req, res) => {
+    res.render('register_login', {
+        route: 'login'
+    });
+});
+
+router.post('/login', (req, res) => {
+    axios.post('/auth/login', {
+        ...req.body
     })
+    .then(response => {
+        if (response.status === 200){
+            res.redirect('/')
+        }
+        else{
+            res.sendStatus(response.status);
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.sendStatus(500);
+    });
 });
 
 module.exports = router;
