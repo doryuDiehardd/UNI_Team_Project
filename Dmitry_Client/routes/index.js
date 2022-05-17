96f3c11c394f2473297ff3540d2014d236c3ec3b
khcodesstuff <dmitry.kh4@gmail.com>
2022-05-17 15:57:56 +0300

    Setup css for dashboard, now rendering dashboard on /, changed css folder structure

--- Dmitry_Client/routes/index.js
+++ Dmitry_Client/routes/index.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    if (!req.session.user){
-        res.redirect('/auth/register');
-    }
+    // if (!req.session.user){
+    //     res.redirect('/auth/register');
+    // }
     
     // show dashboard
+    res.render('dashboard');
 });
 
 module.exports = router;
