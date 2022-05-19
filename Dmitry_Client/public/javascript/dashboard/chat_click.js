ef1a154aa797dd746049cf87fb14900f8dd587fb
khcodesstuff <dmitry.kh4@gmail.com>
2022-05-19 14:25:35 +0300

    Now getting chat data on click

--- Dmitry_Client/public/javascript/dashboard/chat_click.js
+++ Dmitry_Client/public/javascript/dashboard/chat_click.js
@@ -2,8 +2,11 @@ function init_chat_blocks_action_events(){
     const chat_blocks = Array.from(document.querySelectorAll('.chat'));
 
     chat_blocks.forEach(chat_block => {
-        chat_block.addEventListener('click', () => {
-            console.log('click');
+        chat_block.addEventListener('click', async function () {
+            const raw_chat_data = await fetch(`http://localhost:5000/chat/${ this.getAttribute('data-chat-id') }`);
+            const chat_data = await raw_chat_data.json();
+
+            console.log(chat_data);
         });
     });
 }
