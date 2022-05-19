dbb5e4d273ceed3d7e00d6fa5a436fa2f5cedd38
khcodesstuff <dmitry.kh4@gmail.com>
2022-05-19 11:10:48 +0300

    Now displaying all related chats

--- Dmitry_Client/routes/index.js
+++ Dmitry_Client/routes/index.js
@@ -1,12 +1,23 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
+const axios = require('axios').default;
+
+router.get('/', async (req, res) => {
     // if (!req.session.user){
     //     res.redirect('/auth/register');
     // }
     
-    // show dashboard
-    res.render('dashboard');
+    // get all chats related to user
+    // render dashboard with those chats
+
+    // ! Placeholder user
+    const chats_get_result = await axios.get('/chat/related_to/62482ad51f5d80e4751e93cf');
+
+    // console.log(chats_get_result.data.chats[0]);
+
+    res.render('dashboard', {
+        chats: chats_get_result.data.chats
+    });
 });
 
 module.exports = router;
